1bb71335796a65ade22efeb57f252d0f8dc870c2
Xavi Carrillo <xavi.carrillo@gmail.com>
2014-07-14 13:01:19 +0200

    Add first options

--- puppeteer.py
+++ puppeteer.py
@@ -3,7 +3,7 @@
 #
 
 """
-Simple puppetdb wrapper
+Simple puppetdb client
 It uses the pypuppetdb library: https://github.com/nedap/pypuppetdb
 """
 
@@ -18,17 +18,30 @@ __licence__ ="""
 
 usage = """
 %prog [options]
-
 Use --help to view options
 """
 
 from pypuppetdb import connect
+from optparse import OptionParser
+import sys
 
 def main():
-  db = connect()
-  nodes = db.nodes()
-  for node in nodes:
-    print(node)
+
+  parser = OptionParser(usage, version=__version__)
+  parser.add_option("-L", "--licence", action="store_true", default=False, help="Display license information and exit")
+  parser.add_option("-l", "--list", action="store_true", default=False, help="Get the list of all nodes")
+  (options, args) = parser.parse_args()
+
+  if options.licence:
+    print '\n'
+    print __title__, __version__
+    print __licence__
+    sys.exit(0)
+  if options.list:
+    db = connect()
+    nodes = db.nodes()
+    for node in nodes:
+      print(node)
 
 if __name__ == "__main__":
   main()
