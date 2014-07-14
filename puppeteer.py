6ef7beb0bea79887e5692ef5ed0df1391795e7af
Xavi Carrillo <xavi.carrillo@gmail.com>
2014-07-14 15:05:17 +0200

    Show given facts on all hosts

--- puppeteer.py
+++ puppeteer.py
@@ -28,20 +28,25 @@ import sys
 def main():
 
   parser = OptionParser(usage, version=__version__)
-  parser.add_option("-L", "--licence", action="store_true", default=False, help="Display license information and exit")
-  parser.add_option("-l", "--list", action="store_true", default=False, help="Get the list of all nodes")
+  #parser.add_option("-l", "--list", help="Get the list of all nodes")
+  parser.add_option("-f", "--facts", type="string", action="store", dest="facts",
+  help="Get fact from given node")
   (options, args) = parser.parse_args()
+  
+  factsout = ""
+  db = connect()
+  #if options.list:
+  nodes = db.nodes()
 
-  if options.licence:
-    print '\n'
-    print __title__, __version__
-    print __licence__
-    sys.exit(0)
-  if options.list:
-    db = connect()
-    nodes = db.nodes()
+  if options.facts:
+    facts = options.facts.split(',')
     for node in nodes:
-      print(node)
+      for fact in facts:
+        factsout = factsout + " " + node.fact(fact).value
+      print '%s: %s' %(node,factsout)
+      factsout = ""
+
+  sys.exit(0)
 
 if __name__ == "__main__":
   main()
